5110e681790b4dbb36c36a3f8563eeb802d68d1d
Adrien Maranville <adrien.maranville@gmail.com>
2017-03-25 20:24:10 -0600

    Added log out functionality

--- Login Test/ViewController.swift
+++ Login Test/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 import CoreData
 
 class ViewController: UIViewController {
+    
+    var isLoggedIn = false
 
     @IBOutlet weak var lblUsername: UILabel!
     @IBOutlet weak var inputName: UITextField!
@@ -19,17 +21,79 @@ class ViewController: UIViewController {
     @IBAction func btnSubmitPressed(_ sender: Any) {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         let context = appDelegate.persistentContainer.viewContext
-        let newValue = NSEntityDescription.insertNewObject(forEntityName: "Users", into: context)
-        newValue.setValue(inputName.text, forKey: "name")
+        
+        if isLoggedIn {
+            let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Users")
+            do {
+                let results = try context.fetch(request)
+                
+                if results.count > 0 {
+                    for result in results as! [NSManagedObject]{
+                        result.setValue(inputName.text, forKey: "name")
+                        
+                        do {
+                            try context.save()
+                        } catch {
+                            print("Update username save failed")
+                        }
+                        
+                    }
+                lblMessage.text = "Welcome " + inputName.text! + "!"
+                }
+            } catch {
+                print("Update username failed")
+            }
+        } else {
+            let newValue = NSEntityDescription.insertNewObject(forEntityName: "Users", into: context)
+            newValue.setValue(inputName.text, forKey: "name")
+            do {
+                try context.save()
+                lblUsername.text = "Update username"
+                lblMessage.alpha = 1
+                lblMessage.text = "Welcome " + inputName.text! + "!"
+                btnLogOut.alpha = 1
+                btnSubmit.setTitle("Update", for: [])
+                
+                isLoggedIn = true
+            } catch {
+                print("Failed to save")
+            }
+            
+        }
+        
+    }
+    
+    @IBOutlet weak var btnLogOut: UIButton!
+    @IBAction func btnLogOutPressed(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Users")
+        
         do {
-            try context.save()
-            inputName.alpha = 0
-            btnSubmit.alpha = 0
-            lblUsername.alpha = 0
-            lblMessage.alpha = 1
-            lblMessage.text = "Welcome " + inputName.text! + "!"
+            let results = try context.fetch(request)
+            if results.count > 0 {
+                for result in results as! [NSManagedObject] {
+                    context.delete(result)
+                    do {
+                        try context.save()
+                    } catch {
+                        print("Individual delete failed.")
+                    }
+                }
+                lblUsername.text = "Enter username"
+                lblMessage.alpha = 0
+                btnLogOut.alpha = 0
+                btnSubmit.setTitle("Submit", for: [])
+                inputName.alpha = 1
+                btnSubmit.alpha = 1
+                lblUsername.alpha = 1
+                
+                isLoggedIn = false
+
+            }
         } catch {
-            print("Failed to save")
+            print("Delete failed")
         }
     }
     
@@ -42,17 +106,20 @@ class ViewController: UIViewController {
         
         let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Users")
         
+        
+        
         request.returnsObjectsAsFaults = false
         
         do {
             let results = try context.fetch(request)
             for result in results as! [NSManagedObject] {
                 if let username = result.value(forKey: "name") as? String {
-                    inputName.alpha = 0
-                    btnSubmit.alpha = 0
-                    lblUsername.alpha = 0
+                    lblUsername.text = "Update username"
                     lblMessage.alpha = 1
                     lblMessage.text = "Welcome " + username + "!"
+                    btnLogOut.alpha = 1
+                    btnSubmit.setTitle("Update", for: [])
+                    isLoggedIn = true
                 }
             }
         } catch {
